5188607b05688a8a84c289692235300b99615e0f
Ednildo <nildo.777@gmail.com>
2018-08-22 09:44:45 -0300

    on insert sorteio

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -5,6 +5,33 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+exports.onInsertSorteio = functions.database.ref('sorteios').onUpdate(change => {
+    const after = change.after.val();
+    console.log("novo sorteio");
+    const payload = {
+        data: {
+            nome: "after.texto",
+            estabelecimento: "after.estabelecimento"
+        }
+    };
+    const message = {
+        topic: 'sorteios',
+        notification: {
+            body: 'payload.data.nome',
+            title: 'payload.data.estabelecimento'
+        }
+    };
+    //const message:string = "Testando "+payload.data.nome;
+    return admin.messaging().send(message)
+        .catch(err => {
+        console.error('FCM error', err);
+    });
+    /*
+return admin.messaging().sendToTopic('topico',payload,{priority: 'high'})
+    .catch(err => {
+        console.error('FCM error',err);
+    })*/
+});
 exports.getEstabelecimento = functions.https.onRequest((request, response) => {
     const promise = admin.firestore().doc('estabelecimentos/TFl30HUUPumOZ2tMnv4f').get();
     promise.then(snapshot => {
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,34 @@ admin.initializeApp();
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
+export const onInsertSorteio = functions.database.ref('sorteios').onUpdate(change => {
+    const after = change.after.val()
+    console.log("novo sorteio")
+    const payload = {
+        data: {
+            nome: "after.texto",
+            estabelecimento: "after.estabelecimento"
+        }
+    }
+    const message: admin.messaging.Message = {
+        topic: 'sorteios',
+        notification: {
+            body: 'payload.data.nome',
+            title: 'payload.data.estabelecimento'
+        }
+    }
+    //const message:string = "Testando "+payload.data.nome;
+    return admin.messaging().send(message) 
+            .catch(err => {
+                console.error('FCM error',err);
+            })
+            /*
+    return admin.messaging().sendToTopic('topico',payload,{priority: 'high'}) 
+            .catch(err => {
+                console.error('FCM error',err);
+            })*/
+})
+
 export const getEstabelecimento = functions.https.onRequest((request, response) => {
     const promise = admin.firestore().doc('estabelecimentos/TFl30HUUPumOZ2tMnv4f').get();
     promise.then(snapshot => {
